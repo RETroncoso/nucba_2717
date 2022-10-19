20ad0e00869eff9442cf7aac240b0a4b64018693
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2022-10-18 21:48:15 -0300

    Javascript 1

---
+++ Javascript/clase_01/main.js
@@ -0,0 +1,88 @@
+let numeroA = 157;
+let numeroB = 238;
+
+let resultadoNumero = numeroA + numeroB;
+
+// console.log(resultadoNumero);
+
+let textoA = "Harry";
+let textoB = "Potter";
+
+let resultadoTexto = textoA + " " + textoB;
+
+// console.log(resultadoTexto);
+
+// console.log(
+// 	"El nombre de la pelicula es: " +
+// 		resultadoTexto +
+// 		" " +
+// 		"El resultado de la suma es: " +
+// 		resultadoNumero
+// );
+
+// Tipos de datos
+
+// -Number = 1 / 3.56 / 543265465 / NaN
+// -Strings = 'Hola' / "Hola" / `Hola`
+// -Booleanos = true / false
+// -undefined = algo indefinido
+// -null = vacio
+// -funciones
+// -objetos
+
+// Operadores
+
+// + = suma
+// - = resta
+// * = multiplicacion
+// / = division
+// ** = calcular la base a la potencia de exponente
+// % = Módulo. nos da el resto (lo que sobra de una división)
+// ++ = incrementador. Sumar 1
+// -- = decrementador. Restar 1
+
+let numeritoUno = 10;
+let numeritoDos = 5;
+
+// console.log(numeritoUno + numeritoDos);
+// console.log(numeritoUno * numeritoDos);
+// console.log(numeritoUno % numeritoDos);
+// console.log(numeritoUno / numeritoDos);
+
+let saludo = "Hola, mi nombre es ";
+let nombre = "Ruben";
+
+// console.log(saludo + nombre);
+
+// console.log(`${saludo} ${nombre}`);
+
+// console.log(
+// 	`El nombre de la pelicula es: ${resultadoTexto} El resultado de la suma es: ${resultadoNumero}`
+// );
+
+// let dato = prompt("Como te llamas?");
+
+// alert(`Tu nombre, entonces, es ${dato}`);
+
+// Operadores con comparacion
+// == = comparar los valores
+// === = comparar los valores y el tipo de dato
+// != = diferentes valores
+// !== = diferente valor y diferente tipo de dato
+// > mayor que
+// < menor que
+// >= mayor o igual
+// <= menor o igual
+// && = y - and
+// || = o - or
+// ! = negar algo
+
+// console.log(!5 === 5);
+
+// Tipos de datos
+
+console.log(typeof 5);
+console.log(typeof "5");
+console.log(typeof 5.3);
+console.log(typeof true);
+console.log(typeof textoA);
