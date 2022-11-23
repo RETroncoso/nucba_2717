1287fd635e93bd6deeb4bd7985ba738e20f69536
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2022-11-22 21:45:17 -0300

    todo

---
+++ Javascript/clase_11/main.js
@@ -0,0 +1,56 @@
+const input = document.querySelector(".input-text");
+const addForm = document.querySelector(".add-form");
+const tasksList = document.querySelector(".tasks-list");
+const deleteBtn = document.querySelector(".deleteAll-btn");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const saveLocalStorage = (tasksList) => {
+	localStorage.setItem("tasks", JSON.stringify(tasksList));
+};
+
+const createTask = (task) => {
+	`<li>${task.name}<img class="delete-btn" src="./img/delete.svg" alt="boton de borrar" data-name="${task.name}"></li>`;
+};
+
+const renderTasksList = (todoList) => {
+	tasksList.innerHTML = todoList.map((task) => createTask(task).join(""));
+};
+
+const hideDeleteAll = (tasksList) => {
+	if (!tasksList.length) {
+		deleteBtn.classList.add("hidden");
+		return;
+	}
+	deleteBtn.classList.remove("hidden");
+};
+
+const addTask = (e) => {
+	e.preventDefault();
+	const taskName = input.value.trim().replace(/\s+/g, " ");
+	if (!taskName.length) {
+		alert("Por favor, ingrese una tarea");
+		return;
+	} else if (
+		tasks.some((task) => {
+			task.name.toLowerCase() === taskName.toLowerCase();
+		})
+	) {
+		alert("Ya existe una tarea con ese nombre");
+		return;
+	}
+
+	tasks = [...tasks, { name: taskName }];
+	input.value = "";
+	renderTasksList(tasks);
+	saveLocalStorage(tasks);
+	hideDeleteAll(tasks);
+};
+
+const init = () => {
+	renderTasksList(tasks);
+	addForm.addEventListener("submit", addTask);
+	hideDeleteAll(tasks);
+};
+
+init();
